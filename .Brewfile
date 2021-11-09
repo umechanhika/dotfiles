f4bb4fae3a1916fde57ffd697788ca4c896bfb68
umechanhika <hikaru.for.hobby@gmail.com>
2021-11-09 09:26:14 +0900

    Update dotfiles.

---
+++ .Brewfile
@@ -0,0 +1,10 @@
+# tap
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+
+# cask
+cask "google-chrome"
+cask "inkdrop"
+cask "bettertouchtool"
+cask "iterm2"
--- .bin/install.sh
+++ .bin/install.sh
@@ -1,2 +1,7 @@
 # .bash_profile
 ln -s ~/dotfiles/.bash/.bash_profile ~
+
+# brew
+ln -s ~/dotfiles/.Brewfile ~
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle
